d39d1a1e469a604f1238143d01bb24808d9e5b63
ChrisWithakay42 <codewithakay@gmail.com>
2023-05-08 15:09:09 +0100

    rehaul

--- DailyChallengesLevel1/find_pivot_index.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def pivotIndex(self, nums: list[int]) -> int:
-        total_sum = sum(nums)
-        left_sum = 0
-        for index in range(len(nums)):
-            if left_sum == total_sum - left_sum - nums[index]:
-                return index
-            left_sum += nums[index]
-        return -1
--- DailyChallengesLevel1/test_find_pivot_index.py
+++
@@ -1,6 +0,0 @@
-from find_pivot_index import Solution
-
-def test_find_pivot_index():
-    solution = Solution()
-    nums = [1,7,3,6,5,6]
-    assert solution.pivotIndex(nums) == 3

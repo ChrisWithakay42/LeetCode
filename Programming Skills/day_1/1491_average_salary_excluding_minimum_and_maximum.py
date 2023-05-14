67b6c2e256b15f7b9e277e0ec295e6210409b86b
ChrisWithakay42 <codewithakay@gmail.com>
2023-05-14 09:20:10 +0100

    programming skills day 1 task. EASY

--- Programming Skills/day_1/1491_average_salary_excluding_minimum_and_maximum.py
+++ Programming Skills/day_1/1491_average_salary_excluding_minimum_and_maximum.py
@@ -3,4 +3,4 @@ from typing import List
 
 class Solution:
     def average(self, salary: List[int]) -> float:
-        return
+        return (sum(salary) - max(salary) - min(salary)) / (len(salary) - 2)

f9bce3c86426fe6495d0ad40c86e9cc6ae90bb53
ChrisWithakay42 <codewithakay@gmail.com>
2023-05-15 08:05:07 +0100

    functional solution seem to have poor time complexity

--- Programming Skills/day_2/1281_subtract_the_product_and_sum_of_digits_of_an_integer.py
+++ Programming Skills/day_2/1281_subtract_the_product_and_sum_of_digits_of_an_integer.py
@@ -5,4 +5,10 @@ from operator import mul
 class Solution:
     def subtractProductAndSum(self, n: int) -> int:
         digits = [int(digit) for digit in str(abs(n))]
-        return reduce(mul, digits) - sum(digits)
+        prod, su = 1, 0
+        for d in digits:
+            prod *= d
+            su += d
+        return prod - su
+        # The below functional approach has a time complexity
+        # return reduce(mul, digits) - sum(digits)

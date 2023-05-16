77115792779fe3fc654263faa17bf5c201770456
ChrisWithakay42 <codewithakay@gmail.com>
2023-05-16 09:42:18 +0100

    this was OK

---
+++ Programming Skills/day_4/1822_sign_of_the_product_of_an_array.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+class Solution:
+    def arraySign(self, nums: List[int]) -> int:
+        product = 1
+        for i in nums:
+            product *= i
+        if product > 0: return 1
+        elif product < 0: return -1
+        else: return 0

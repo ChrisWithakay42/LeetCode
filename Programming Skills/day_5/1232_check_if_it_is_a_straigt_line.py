8c11c8b635e39448047f4d009581918b6eccd826
ChrisWithakay42 <codewithakay@gmail.com>
2023-05-17 22:59:45 +0100

    kwl task

---
+++ Programming Skills/day_5/1232_check_if_it_is_a_straigt_line.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+class Solution:
+    def checkStraightLine(self, coordinates: Listt[List[int]]) -> bool:
+        if len(coordinates) < 3:
+            return True
+
+        x0, y0 = coordinates[0]
+        x1, y1 = coordinates[1]
+        initial_slope = (y1 - y0) / (x1 - x0) if (x1 - x0) != 0 else float('inf')
+
+        for i in range(2, len(coordinates)):
+            xi, yi = coordinates[i]
+            slope = (yi - y0) / (xi - x0) if (xi - x0) != 0 else float('inf')
+            if slope != initial_slope:
+                return False
+
+        return True

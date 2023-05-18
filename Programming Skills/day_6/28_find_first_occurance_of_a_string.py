6ed2fdca5a04ebb61cf63aa659582f67601217d4
ChrisWithakay42 <codewithakay@gmail.com>
2023-05-18 13:31:05 +0100

    commented solution is Python lols

---
+++ Programming Skills/day_6/28_find_first_occurance_of_a_string.py
@@ -0,0 +1,17 @@
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        # return haystack.find(needle)
+        haystack_len = len(haystack)
+        needle_len = len(needle)
+
+        if needle_len > haystack_len:
+            return -1
+
+        if needle_len == 0:
+            return 0
+
+        for i in range(haystack_len - needle_len + 1):
+            if haystack[i:i + needle_len] == needle:
+                return i
+
+        return -1

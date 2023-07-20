66ef6980874e21bb46e17e4317934fd064dab9f6
ChrisWithakay42 <codewithakay@gmail.com>
2023-07-20 12:48:00 +0100

    medium

---
+++ SQL/freeform/1633. Percentage of Users Attended a Contest.sql
@@ -0,0 +1,10 @@
+SELECT
+contest_id,
+ROUND(COUNT(DISTINCT user_id) / (SELECT COUNT(DISTINCT user_id) FROM Register) * 100, 2) AS percentage
+FROM
+  Register
+GROUP BY
+  contest_id
+ORDER BY
+  percentage DESC,
+  contest_id ASC;
